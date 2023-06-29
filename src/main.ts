be13d02297d64cd734ea84ed4e9b800535116cc8
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-06-29 08:38:41 -0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Payever api')
+  .setDescription('')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe());
+  SwaggerModule.setup('api/doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
